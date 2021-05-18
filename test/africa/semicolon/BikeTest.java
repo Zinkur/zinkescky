a927537beb9d389dffd2be3e08403f69c3e56745
Zinkur Mirigazhi <zinkurmirigazhi@gmail.com>
2021-05-18 08:49:15 +0100

    added aircondition

---
+++ test/africa/semicolon/BikeTest.java
@@ -0,0 +1,28 @@
+package africa.semicolon;
+
+import africa.semicolon.chapterThree.Bike;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BikeTest2 {
+    @Test
+    public void testThatAccelerationCanBeSet(){
+        // given
+        Bike2 okada = new Bike ();
+        // when
+        okada.setMaxAccelerationInKmPerHour(20);
+        // confirm
+        assertEquals(20, okada.getMaxAccelerationInKmPerHour());
+        public boolean IsOn
+                if(isOn)
+
+                    this.IsOn=IsOn;
+                if (this.isOn) {
+                    current GearNumber 1;
+                }
+                if (!this.isOn)
+                    current GearNumber=0;
+                }
+    }
+}
