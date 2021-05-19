76c41f25b9dfa2868fcb98378f9ef5f681828822
Zinkur Mirigazhi <zinkurmirigazhi@gmail.com>
2021-05-19 18:36:22 +0100

    Added ElseStatement

--- test/africa/semicolon/ElseStatement.java
+++ test/africa/semicolon/ElseStatement.java
@@ -1,2 +1,27 @@
-package africa.semicolon;public class ElseStatement {
+package africa.semicolon;
+
+import java.util.Scanner;
+
+public class ElseStatement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Student Score: ");
+        int score;
+        score = scanner.nextInt();
+        if (score >= 90) {
+            System.out.println("A");
+        }else
+            if (score >= 80) {
+                System.out.println("B");
+            }else
+                if (score >= 70){
+            System.out.println("C");
+        }else
+            if (score >= 60){
+            System.out.println("D");
+        }
+            if (score < 60) {
+                System.out.println("F");
+            }
+    }
 }
